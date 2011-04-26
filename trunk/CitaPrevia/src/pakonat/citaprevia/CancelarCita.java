1c3d7cf64397f304ff290f2d96ecea8ed4739031
packarbell@gmail.com <packarbell@gmail.com@bd0c1af5-e5be-0c9e-6510-86ae0700e12a>
2011-04-26 16:53:59 +0000

    lsitado de citas en boton

--- trunk/CitaPrevia/src/pakonat/citaprevia/CancelarCita.java
+++ trunk/CitaPrevia/src/pakonat/citaprevia/CancelarCita.java
@@ -1,20 +1,35 @@
 package pakonat.citaprevia;
 
+import pakonat.citaprevia.html.beans.InfoCita;
 import pakonat.citaprevia.main.threads.ListarCitasThread;
 import pakonat.citaprevia.utils.ActividadMenu;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class CancelarCita extends ActividadMenu {
 	
 	static ProgressBar loading;
 	static LinearLayout capaLoading;
+	static LinearLayout capaInfo;
+	static Context context;
+	static InfoCita[] listaCitas;
 	
-    @Override
+    public static InfoCita[] getListaCitas() {
+		return listaCitas;
+	}
+
+	public static void setListaCitas(InfoCita[] listaCitas) {
+		CancelarCita.listaCitas = listaCitas;
+	}
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cancelar_cita);
@@ -25,18 +40,35 @@ public class CancelarCita extends ActividadMenu {
     }
     
 	private void inicializaObjetos() {
+		
+		context=this.getApplicationContext();
 
     	loading=(ProgressBar) findViewById(R.id.cargandoNuevaCita);
     	capaLoading=(LinearLayout) findViewById(R.id.linearLayout2);
+		capaInfo=(LinearLayout) findViewById(R.id.capaInfo);
 		
 	}
     
     public static final Runnable mUpdateResults = new Runnable() {
         public void run() {
         	loading.setVisibility(View.INVISIBLE);        	
-        	//Pinto los resultados
-        	
+        	//Pinto los resultados        	
+        	capaInfo.addView(getRadioGroup(listaCitas));
         }
     };
+    
+    private static RadioGroup getRadioGroup(InfoCita[] listaCitas2){
+    	RadioGroup rg=new RadioGroup(context);
+    	
+    	RadioButton[] rb = new RadioButton[10];
+        for(int i=0; i<listaCitas2.length; i++){
+            rb[i]  = new RadioButton(context);
+            rg.addView(rb[i]); 
+            rb[i].setText(listaCitas2[i].getDia()+" "+listaCitas2[i].getHora());
+         }
+        
+        return rg;
+
+    }
 
 }
